2cc970d36b678106dcda9dcdcbbf2b539dea73cf
Em Karisch <em@khaos.solutions>
2025-09-17 23:14:46 +0200

    fix: move SchallCircuitGroup integration to data-final-fixes phase (#2)

--- data-final-fixes/schall-circuit-group.lua
+++ data-final-fixes/schall-circuit-group.lua
@@ -1,3 +1,4 @@
 if mods["SchallCircuitGroup"] then
   data.raw.item["cybersyn-constant-combinator"].subgroup = "circuit-combinator"
+  data.raw.recipe["cybersyn-constant-combinator"].subgroup = "circuit-combinator"
 end
